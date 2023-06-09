374381ed0d8b71e7c9311e795aa37f1cba1dd55e
ngovansap <ngovansap2k@gmail.com>
2023-06-09 17:41:38 +0700

    fix body

--- app/Console/Commands/ExtractCategoryPost.php
+++ app/Console/Commands/ExtractCategoryPost.php
@@ -2,14 +2,13 @@
 
 namespace App\Console\Commands;
 
-use Goutte\Client;
-use App\Models\Post;
 use App\Models\Category;
-use Illuminate\Support\Str;
 use App\Models\CategoryPost;
+use App\Models\Post;
+use Goutte\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Str;
 
 class ExtractCategoryPost extends Command
 {
@@ -38,20 +37,20 @@ class ExtractCategoryPost extends Command
     }
 
     /*
-    *  Author : Sap
-    *  Date Create : 24/03/2023
-    *  Description : Create Category
-    *  Parameter :
-    *  Return : create Category, $categoryIds
-    */
+     *  Author : Sap
+     *  Date Create : 24/03/2023
+     *  Description : Create Category
+     *  Parameter :
+     *  Return : create Category, $categoryIds
+     */
     public function extractCategory()
     {
 
         $client = new Client();
         $crawler = $client->request('GET', 'https://people.com/parents/hoda-kotb-absent-from-today-as-she-spends-spring-break-with-daughters-after-hope-health-scare/');
 
-        $getURl = $crawler->filter('.mntl-breadcrumbs .mntl-breadcrumbs__item')->each(function ($node) use ($client , $crawler) {
-            return  $node->filter('a')->attr('href');
+        $getURl = $crawler->filter('.mntl-breadcrumbs .mntl-breadcrumbs__item')->each(function ($node) use ($client, $crawler) {
+            return $node->filter('a')->attr('href');
         });
 
         $categoryIds = array();
@@ -70,9 +69,9 @@ class ExtractCategoryPost extends Command
                 'order' => 123,
             ]);
 
-            $crawlerLast = $client->request('GET',$lastUrl);
+            $crawlerLast = $client->request('GET', $lastUrl);
             $urlPosts = $crawlerLast->filter('.card--no-image')->each(function ($node) use ($client) {
-                return  $node->attr('href');
+                return $node->attr('href');
             });
 
             $category[] = $newCategory;
@@ -80,67 +79,75 @@ class ExtractCategoryPost extends Command
 
         }
 
-        for($i = 0 ; $i < count($urlPosts); $i++) {
+        for ($i = 0; $i < count($urlPosts); $i++) {
             $urlPost = $urlPosts[$i];
             $crawler = $client->request('GET', $urlPost);
             $title = $crawler->filter(' .people-article .article-header h1')->text();
             $slug = Str::slug($title);
             $currentUrl = $client->getHistory()->current()->getUri();
-                $body =  $crawler->filter('.structured-content p')->each(function ($node){
-                    return $node->text();
+            $body = $crawler->filter('.structured-content p')->each(function ($node) {
+                $node->filter('a')->each(function ($anchorNode) {
+                    $anchorNode->getNode(0)->parentNode->removeChild($anchorNode->getNode(0));
                 });
-                $stringBody = implode(", ", $body);
-                $imageUrl = $crawler->filter('img')->first()->attr('src');
-                $slugImage = Str::slug(pathinfo($imageUrl, PATHINFO_FILENAME));
-                $file_name = $slugImage . '.' . pathinfo($imageUrl, PATHINFO_EXTENSION);
-                if (!Storage::exists('public/images/' . $file_name)) {
-                    $imageContent = file_get_contents($imageUrl);
-                    Storage::put('public/images/' . $file_name, $imageContent);
+                return '<p>' . $node->html() . '</p>';
+            });
+            $stringBody = implode("\n", $body);
+            $imageUrl = $crawler->filter('img')->first()->attr('src');
+            $slugImage = Str::slug(pathinfo($imageUrl, PATHINFO_FILENAME));
+            $file_name = $slugImage . '.' . pathinfo($imageUrl, PATHINFO_EXTENSION);
+            if (!Storage::exists('public/images/' . $file_name)) {
+                $imageContent = file_get_contents($imageUrl);
+                Storage::put('public/images/' . $file_name, $imageContent);
+            }
+
+            $post = Post::where('slug', $slug)->updateOrCreate([
+                'author_id' => 1,
+                'category_id' => null,
+                'title' => $title,
+                'body' => $stringBody,
+                'image' => $file_name,
+                'slug' => $slug,
+                'status' => 1,
+                'featured' => 0,
+                'source' => $currentUrl,
+            ]);
+
+            $postId = $post->id;
+
+            foreach ($categoryIds as $categoryId) {
+                $exist = CategoryPost::where('category_id', $categoryId)->where('post_id', $postId)->first();
+                if (!$exist) {
+                    CategoryPost::updateOrCreate([
+                        'category_id' => $categoryId,
+                        'post_id' => $postId,
+                    ]);
                 }
-
-                $post = Post::where('slug', $slug)->updateOrCreate([
-                    'author_id' => 1,
-                    'category_id'=> null,
-                    'title'    => $title,
-                    'body'   => $stringBody,
-                    'image' => $file_name,
-                    'slug' => $slug,
-                    'status' => 1,
-                    'featured'=> 0,
-                    'source' => $currentUrl,
-                ]);
-
-                $postId = $post->id;
-
-                foreach ($categoryIds as $categoryId) {
-                    $exist = CategoryPost::where('category_id', $categoryId)->where('post_id', $postId)->first();
-                    if (!$exist) {
-                        CategoryPost::updateOrCreate([
-                            'category_id' => $categoryId,
-                            'post_id' => $postId,
-                        ]);
-                    }
-                }
+            }
         }
 
         return $categoryIds;
     }
     /*
-    *  Author : Sap
-    *  Date Create : 27/03/2023
-    *  Description : Create Post
-    *  Parameter :
-    *  Return : create Post, create CategoryPost
-    */
+     *  Author : Sap
+     *  Date Create : 27/03/2023
+     *  Description : Create Post
+     *  Parameter :
+     *  Return : create Post, create CategoryPost
+     */
     public function extractPost()
     {
         $client = new Client();
         $crawler = $client->request('GET', 'https://people.com/parents/hoda-kotb-absent-from-today-as-she-spends-spring-break-with-daughters-after-hope-health-scare/');
         $title = $crawler->filter(' .people-article .article-header h1')->text();
 
-        $body =  $crawler->filter('.structured-content p')->each(function ($node){
-            return $node->text();
+        $body = $crawler->filter('.structured-content p')->each(function ($node) {
+            $node->filter('a')->each(function ($anchorNode) {
+                $anchorNode->getNode(0)->parentNode->removeChild($anchorNode->getNode(0));
+            });
+            return '<p>' . $node->html() . '</p>';
         });
+
+        $bodyText = implode("\n", $body);
         $slug = Str::slug($title);
         $currentUrl = $client->getHistory()->current()->getUri();
         $stringBody = implode(", ", $body);
@@ -153,13 +160,13 @@ class ExtractCategoryPost extends Command
         }
         $post = Post::where('slug', $slug)->updateOrCreate([
             'author_id' => 1,
-            'category_id'=> null,
-            'title'    => $title,
-            'body'   => $stringBody,
+            'category_id' => null,
+            'title' => $title,
+            'body' => $bodyText,
             'image' => $file_name,
             'slug' => $slug,
             'status' => 1,
-            'featured'=> 0,
+            'featured' => 0,
             'source' => $currentUrl,
         ]);
 
